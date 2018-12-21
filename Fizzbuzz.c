6b470ad42ced6695aa84d1950772419c3b349556
Adriana Jimenez <45694899+adrianaj24@users.noreply.github.com>
2018-12-21 16:05:47 -0500

    Update Fizzbuzz.c

--- Fizzbuzz.c
+++ Fizzbuzz.c
@@ -13,7 +13,7 @@ int main () {
     int i;
     for (i = 1; i <= 100; i++) {
         
-        if ((i % 3) && (i % 5)) {
+        if ((i % 3 == 0) && (i % 5 == 0)) {
             printf("FizzBuzz\n");
         } else if (i % 3 == 0) {
             printf("Fizz\n");
